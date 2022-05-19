12ac2208bfe5abc807f02ff5d60224c268fd4bc7
Sharmishameela <sharmishameela@gmail.com>
2022-05-19 17:26:24 +0530

    Done

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	public void deposit()
+	{
+		System.out.println("Deposit amount 100000");
+	}
+	public static void main(String[] args) {
+		AxisBank a=new AxisBank();
+		a.savings();
+		a.fixed();
+		a.deposit();
+		
+	}
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,27 @@
+/*QUESTION 9:
+------------
+     Project   :BankDetails
+     Package   :org.bank
+     Class     :BankInfo
+     Methods   :saving(),fixed()
+
+     Class     :AxisBank
+     Methods   :deposit()
+
+Description:
+create above 2 class and call all your class methods into the BankInfo using single inheritance.*/
+
+package org.bank;
+
+public class BankInfo {
+	public void savings()
+	{
+		System.out.println("Savings Account");
+	}
+	public void fixed()
+	{
+		System.out.println("Fixed Deposit");
+	
+	}
+
+}
